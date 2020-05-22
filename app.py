aa3ce95e2bc1153ef9fad8a1bb4fcde55f04658a
jasper <jasper_versantvoort@hotmail.com>
2020-05-22 11:33:35 +0200

    print rijen en connectie af

--- app.py
+++ app.py
@@ -3,10 +3,17 @@ import mysql.connector
 
 app = Flask(__name__)
 
+conn = mysql.connector.connect(host='ensembldb.ensembl.org', user='anonymous',
+                               db='homo_sapiens_core_95_38')
+cursor = conn.cursor()
+cursor.execute("select * from gene")
+rows = cursor.fetchall()
+print(rows)
+cursor.close()
+conn.close()
 
 @app.route('/afvink3')
 def Connect_database():
-    conn = mysql.connector.connect(host='ensembldb.ensembl.org',user ='anonymous',db='homo_sapiens_core_95_38')
 
     return 'Hello World!'
 

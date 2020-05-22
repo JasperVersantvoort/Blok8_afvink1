6006bbf7f9ceeff18b024145f19523b4dc95f2e9
jasper <jasper_versantvoort@hotmail.com>
2020-05-22 11:59:36 +0200

    start aan html

--- app.py
+++ app.py
@@ -3,18 +3,19 @@ import mysql.connector
 
 app = Flask(__name__)
 
-conn = mysql.connector.connect(host='ensembldb.ensembl.org', user='anonymous',
-                               db='homo_sapiens_core_95_38')
-cursor = conn.cursor()
-cursor.execute("select * from gene")
-rows = cursor.fetchall()
-print(rows)
-cursor.close()
-conn.close()
+
 
 @app.route('/afvink3')
 def Connect_database():
-
+    conn = mysql.connector.connect(host='ensembldb.ensembl.org',
+                                   user='anonymous',
+                                   db='homo_sapiens_core_95_38')
+    cursor = conn.cursor()
+    cursor.execute("select description from gene")
+    rows = cursor.fetchall()
+    print(rows)
+    cursor.close()
+    conn.close()
     return 'Hello World!'
 
 

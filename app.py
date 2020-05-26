03bd47aec947de3b7002e3c94f47c05169528e09
jasper <jasper_versantvoort@hotmail.com>
2020-05-26 10:10:54 +0200

    Zoekbalk werkend!

--- app.py
+++ app.py
@@ -1,27 +1,39 @@
 from flask import Flask, render_template, request
 import mysql.connector
 
+
 app = Flask(__name__)
 
 
-@app.route('/afvink3', methods=["POST"])
+@app.route('/afvink3', methods=["POST", "GET"])
 def webpagina():
-    rows= connect_database()
-    return render_template("Afvink3.html", database=rows)
+    if request.method =="POST":
+        zoek = request.form.get("zoek", "")
+        rows = connect_database(zoek)
+
+        return render_template("Afvink3.html", database=rows, zoek=zoek)
+
+    else:
+        rows = connect_database("None")
+        return render_template("Afvink3.html", database=rows, zoek="None")
 
 
-def connect_database():
+def connect_database(zoek):
+    print("zoek woord is: ",zoek)
     conn = mysql.connector.connect(host='ensembldb.ensembl.org',
                                    user='anonymous',
                                    db='homo_sapiens_core_95_38')
     cursor = conn.cursor()
     cursor.execute("select description from gene")
-    #rows = cursor.fetchall()
     rows = cursor.fetchall()
-    des =[]
+    des = []
     for row in rows:
+        # print (row)
         if str(row) != "(None,)":
-            des.append(row)
+            # print(row)
+            if zoek in str(row) or zoek is None:
+                des.append(row)
+                # print (des)
 
     cursor.close()
     conn.close()
